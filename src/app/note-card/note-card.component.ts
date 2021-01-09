f96f07000bdf144a68f8ce1ffca42ded8afddd64
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-01-09 19:16:32 +0100

    notesList set

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements OnInit, AfterViewInit {
+  @Input() title : string = '';
+  @Input() body : string = '';
 
-  constructor() { }
+  @ViewChild('truncator')
+  truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText')
+  bodyText!: ElementRef<HTMLElement>;
+
+  showTruncator = false;
+
+  constructor(private cdRef:ChangeDetectorRef) { }
+  ngAfterViewInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+    this.showTruncator = this.bodyText.nativeElement.scrollHeight > viewableHeight;
+    this.cdRef.detectChanges();
+  }
 
   ngOnInit(): void {
   }
