981ca95a2521c6c08d1212c4b86dc1f49ff77982
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-01-11 01:12:30 +0100

    del animation

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,7 +1,7 @@
 import { NotesService } from './../../shared/notes.service';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
-import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
@@ -9,44 +9,58 @@ import { animate, keyframes, style, transition, trigger } from '@angular/animati
   styleUrls: ['./notes-list.component.scss'],
   animations: [
     trigger('myInsertRemoveTrigger', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('1000ms', style({ opacity: 1 })),
+      transition( 'void => *', [
+        // init state
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+          // We have to expand out the padding property
+          paddingTop : 0,
+          paddingBottom : 0,
+          paddingRight : 0,
+          paddingLeft : 0
+        }),
+        // Animate spacing
+        animate(50,
+          style({
+            height: '*',
+            'margin-bottom': '*',
+            paddingTop: '*',
+            paddingBottom : '*',
+            paddingRight : '*',
+            paddingLeft : '*'
+        })),
+        animate(68)
       ]),
-      transition(':leave', [
-        animate('1000ms', style({ opacity: 0 }))
+      transition('* => void', [
+        // First scale up
+        animate(50 , style({transform : 'scale(1.01)'})),
+        // then scale back to normal with fading
+        animate(50 , style({transform : 'scale(1)', opacity : 0.75})),
+        // scale down and fading completely
+        animate('120ms ease-out' , style({transform : 'scale(0.68)', opacity : 0})),
+        // void state
+        animate('150ms ease-out',
+          style({
+            height: 0,
+            'margin-bottom': 0,
+            paddingTop : 0,
+            paddingBottom : 0,
+            paddingRight : 0,
+            paddingLeft : 0
+          })
+        )
       ])
-    ]),
-  ],
-  // animations: [
-  //   trigger('noteAdding', [
-  //     transition( ':enter', [
-  //       // init state
-  //       style({
-  //         height: 0,
-  //         opacity: 0,
-  //         transform: 'scale(0.85)',
-  //         marginBottom: 0,
-  //         // We have to expand out the padding property
-  //         paddingTop : 0,
-  //         paddingBottom : 0,
-  //         paddingRight : 0,
-  //         paddingLeft : 0
-  //       }),
-  //       // Animate spacing
-  //       animate('50ms', keyframes([
-  //         style({
-  //           height: '*',
-  //           marginBottom: '*',
-  //           paddingTop: '*',
-  //           paddingBottom : '*',
-  //           paddingRight : '*',
-  //           paddingLeft : '*'
-  //       })])),
-  //       animate(500)
-  //     ])
-  //   ])
-  // ]
+      // transition(':enter', [
+      //   animate('200ms', style({ opacity: 1 }))
+      // ]),
+      // transition(':leave', [
+      //   animate('200ms', style({ opacity: 0 }))
+      // ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
